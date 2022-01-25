ae471b5578254e0b97c6522ed911c33872eaaa73
Krysenkova <ekaterina94uvaogmail.com>
2022-01-25 08:05:37 +0100

    Initial Commit

---
+++ SelfChat/SelfChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SelfChatApp.swift
+//  SelfChat
+//
+//  Created by Masyschka on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct SelfChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
